5765560c8ae4ddf83403940237455a25f42210b0
dev-from-cbd <veryfatwombat@gmail.com>
2022-06-26 08:18:36 +0930

    Progress Steps

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
